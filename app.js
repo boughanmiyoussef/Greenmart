60a85ab2ad88a26bbdf452cdc186e91bf95178b5
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-10-29 15:27:04 +0100

    Server

--- app.js
+++ app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3000;
 
+const userRoutes = require('./routes/user');
+const orderRoutes = require('./routes/order');
+const plantRoutes = require('./routes/plant');
+const blogRoutes = require('./routes/blog');
+const commentRoutes = require('./routes/comment');
+
+// Middleware for parsing JSON
 app.use(express.json());
-app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-   res.sendFile(__dirname + '/public/index.html');
-});
+app.use('/api/users', userRoutes);
+app.use('/api/orders', orderRoutes);
+app.use('/api/plants', plantRoutes);
+app.use('/api/blogs', blogRoutes);
+app.use('/api/comments', commentRoutes);
 
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-   console.log(`Server running on port ${PORT}`);
+    console.log(`Server running on port ${PORT}`);
 });
