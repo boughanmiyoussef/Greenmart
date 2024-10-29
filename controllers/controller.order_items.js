c008d0d8be570f103eb0ba4e4c60a7c93226d051
boughanmiyoussef <yussefboughanmy@gmail.com>
2024-10-29 15:22:17 +0100

    Order_items Controller

---
+++ controllers/controller.order_items.js
@@ -0,0 +1,26 @@
+// controllers/orderItemController.js
+const OrderItem = require('../models/order_items');
+
+// Add item to order
+exports.addItemToOrder = async (req, res) => {
+    try {
+        const { orderID, plantID, quantity, price } = req.body;
+        const orderItem = new OrderItem(orderID, plantID, quantity, price);
+
+        await orderItem.save();
+        res.status(201).json({ message: "Item added to order", orderItem });
+    } catch (error) {
+        res.status(500).json({ error: "Error adding item to order" });
+    }
+};
+
+exports.getOrderItems = async (req, res) => {
+    try {
+        const orderID = req.params.orderId;
+        const items = await OrderItem.findByOrderId(orderID);
+
+        res.status(200).json(items);
+    } catch (error) {
+        res.status(500).json({ error: "Error fetching order items" });
+    }
+};
